2741570a58e175f273c61523009afc67cb2cc84c
Paul Walker <paul@pwjw.com>
2025-10-15 14:16:15 -0400

    Reorder device id and rename Windows -> WASAPI

--- modules/juce_audio_devices/native/juce_WASAPI_windows.cpp
+++ modules/juce_audio_devices/native/juce_WASAPI_windows.cpp
@@ -1867,7 +1867,7 @@ public:
     Devices devices;
 
 private:
-    DeviceChangeDetector deviceChangeDetector { L"Windows Audio", [this] { systemDeviceChanged(); } };
+    DeviceChangeDetector deviceChangeDetector { L"WASAPI", [this] { systemDeviceChanged(); } };
     WASAPIDeviceMode deviceMode;
     bool hasScanned = false;
     ComSmartPtr<IMMDeviceEnumerator> enumerator;
@@ -2016,9 +2016,9 @@ private:
     //==============================================================================
     static String getDeviceTypename (WASAPIDeviceMode mode)
     {
-        if (mode == WASAPIDeviceMode::shared)            return "Windows Audio";
-        if (mode == WASAPIDeviceMode::sharedLowLatency)  return "Windows Audio (Low Latency Mode)";
-        if (mode == WASAPIDeviceMode::exclusive)         return "Windows Audio (Exclusive Mode)";
+        if (mode == WASAPIDeviceMode::shared)            return "WASAPI";
+        if (mode == WASAPIDeviceMode::sharedLowLatency)  return "WASAPI (Low Latency Mode)";
+        if (mode == WASAPIDeviceMode::exclusive)         return "WASAPI (Exclusive Mode)";
 
         jassertfalse;
         return {};
